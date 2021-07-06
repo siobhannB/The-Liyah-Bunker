ed7ab9ee64d8acc170b9d01d5b34059f1d2823e6
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-07-06 17:31:40 +0800

    Create PlayerController.cs

---
+++ The Liyah Bunker/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+public float moveSpeed = 5f; //controls how fast player moves
+
+public Rigidbody2D rb;
+
+Vector2 movement;
+
+public Animator animator; //references the animation/animator in unity so when player moves,animation plays
+
+   private bool IsRunning;
+    private bool walking;
+
+    // Start is called before the first frame update
+    void Start(){
+    walking = false;
+    }
+    
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        
+         //Idle Facing Directions
+            if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+            {
+                animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+            
+             if (movement.magnitude > 0.3f)
+        {
+            
+            walking = true;
+        }
+        else
+        {
+            if (walking == true)
+            {
+                walking = false;
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
