9ade43c1f5c37719b57087f6c4975cbe55c05650
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-07-06 17:31:34 +0800

    Create CameraController.cs

---
+++ The Liyah Bunker/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; //make sure to drag player into target slot on under camera script
+    //make sure to add pixelsnap material to material in tilemap renderer for pixel art/grid.  
+    public float smoothing;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    private float zoffset;
+    void Start()
+    {
+        zoffset = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, zoffset);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
