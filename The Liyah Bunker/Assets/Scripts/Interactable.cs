70b106991bf86d383bda6e39f1c6ec673cb4e32f
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-07-25 23:37:20 +0800

    Create Interactable.cs

---
+++ The Liyah Bunker/Assets/Scripts/Interactable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Interactable : MonoBehaviour
+{
+    [System.Serializable]
+    public class Interactions : UnityEvent<GameObject>
+    {
+        public GameObject gameObject;
+    }
+    public float radius = 3f;
+
+    public Interactions OnInteract = new Interactions();
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void Interact(GameObject g)
+    {
+        OnInteract.Invoke(g);
+    }
+}
