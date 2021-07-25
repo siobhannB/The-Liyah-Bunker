246d8c34cd2095baa0d360c234c116be3691c90e
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-07-25 23:37:31 +0800

    Create Rock.cs

---
+++ The Liyah Bunker/Assets/Scripts/Rock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Collider2D collider;
+    RockThrower rockThrower;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        rockThrower = FindObjectOfType<RockThrower>();
+    }
+
+
+    public void Throw(Vector2 direction, float strength)
+    {
+        rb2d.AddForce(direction * strength);
+        collider.isTrigger = false;
+    }
+
+    public void BePickedUp()
+    {
+        rockThrower.Pickup(this);
+    }
+}
