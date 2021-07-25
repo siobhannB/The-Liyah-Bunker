bdf2fe06a3f5ddd979c103b199b5ee8b9b98d8b7
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-07-25 23:37:45 +0800

    Create RockThrower.cs

---
+++ The Liyah Bunker/Assets/Scripts/RockThrower.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockThrower : MonoBehaviour
+{
+    private int numRocks;
+    public Rock rockPrefab;
+    public float spawnDist = 0.3f;
+    public float strength = 3.0f;
+    List<Rock> rocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocks = new List<Rock>();
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Reset()
+    {
+        numRocks = 0;
+        while (rocks.Count > 0)
+        {
+            Reactivate();
+        }
+    }
+
+    public void Pickup(Rock r)
+    {
+        rocks.Add(r);
+        r.gameObject.SetActive(false);
+        numRocks += 1;
+    }
+
+    //Reactivate the first rock on the list,
+    //removing it from the list.
+    public void Reactivate()
+    {
+        if (rocks.Count > 0)
+        {
+            Rock r = rocks[0];
+            rocks.Remove(r);
+            r.gameObject.SetActive(true);
+        }
+    }
+
+    //Throw a rock at pos.
+    public void ThrowRock(Vector2 pos)
+    {
+        if (numRocks > 0)
+        {
+            numRocks -=1;
+            //Work out what direction the pos is from us.
+            Vector2 direction = ((Vector2)(pos - (Vector2)transform.position)).normalized;
+            Vector2 spawnpos = (Vector2)(transform.position) + (direction * spawnDist);
+            Debug.Log("Instantiating");
+            Rock r = Instantiate(rockPrefab, spawnpos, Quaternion.identity);
+            Debug.Log("Throwing");
+            r.Throw(direction, strength);
+            Destroy(r.gameObject, 5.0f);
+            Reactivate();
+        }
+    }
+}
