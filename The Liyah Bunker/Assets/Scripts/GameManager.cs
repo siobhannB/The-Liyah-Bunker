38ed60223ab9651baf208071bbeb5773b08e87be
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-08-09 04:21:44 +0800

    Create GameManager.cs

---
+++ The Liyah Bunker/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _instance;
+    static List<YarnProgram> addedPrograms = new List<YarnProgram>();
+    public DialogueRunner dialogueRunner;
+    
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static GameManager instance
+    {
+        get {
+            return _instance;
+        }
+    }
+
+    public void StartDialogue(string node)
+    {
+        dialogueRunner.StartDialogue(node);
+    }
+
+    public void AddDialogue(YarnProgram dscript)
+    {
+        if (!addedPrograms.Contains(dscript))
+        {
+            dialogueRunner.Add(dscript);
+            addedPrograms.Add(dscript);
+        }
+    }
+}
