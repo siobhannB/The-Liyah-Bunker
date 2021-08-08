eeb10bb4f3a705a9c935843bac2929f768878761
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-08-09 04:21:22 +0800

    Create Axe.cs

---
+++ The Liyah Bunker/Assets/Scripts/Axe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Axe : MonoBehaviour
+{
+    [YarnCommand("destroy")]
+    public void Destroy() {
+        Destroy(gameObject);
+    }
+}
