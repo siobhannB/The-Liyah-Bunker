6d256bf4f2cfcbf04c2023f474c03cf93d32a52a
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-08-09 04:21:32 +0800

    Create Dialogue.cs

---
+++ The Liyah Bunker/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class Dialogue : MonoBehaviour
+{
+    public string startNode = "";
+    
+
+    [Header("Optional")]
+    public YarnProgram dialogueScript;
+
+    void Start()
+    {
+        if (dialogueScript != null)
+        {
+            GameManager.instance.AddDialogue(dialogueScript);
+        }
+    }
+
+    public void StartDialogue()
+    {
+        // Kick off the dialogue at this node.
+        GameManager.instance.StartDialogue(startNode);
+    }
+}
