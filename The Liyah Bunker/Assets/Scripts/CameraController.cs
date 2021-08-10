32f0993ec9a7304df37f21a1957bbe1b91548c24
siobhannB <64455423+siobhannB@users.noreply.github.com>
2021-08-10 09:34:55 +0800

    Update CameraController.cs

--- The Liyah Bunker/Assets/Scripts/CameraController.cs
+++ The Liyah Bunker/Assets/Scripts/CameraController.cs
@@ -5,29 +5,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target; //make sure to drag player into target slot on under camera script
-    //make sure to add pixelsnap material to material in tilemap renderer for pixel art/grid.  
-    public float smoothing;
-    public Vector2 minPosition;
-    public Vector2 maxPosition;
+                             //make sure to add pixelsnap material to material in tilemap renderer for pixel art/grid.  
 
-    private float zoffset;
     void Start()
     {
-        zoffset = transform.position.z;
+        
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (transform.position != target.position)
-        {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, zoffset);
-
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-
-
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
-        }
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
+
 }
